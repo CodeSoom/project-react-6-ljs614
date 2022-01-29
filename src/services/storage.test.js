edf53376945297f2ac1753b46a94e80515019eb6
jeongseop.lee <sjl0614@naver.com>
2022-01-29 16:16:06 +0900

    init project

---
+++ src/services/storage.test.js
@@ -0,0 +1,39 @@
+import {
+  saveItem,
+  loadItem,
+  removeItem,
+} from './storage';
+
+describe('storage', () => {
+  jest.spyOn(window.localStorage.__proto__, 'setItem');
+
+  beforeEach(() => {
+    window.localStorage.__proto__.setItem = jest.fn();
+    window.localStorage.__proto__.getItem = jest.fn();
+    window.localStorage.__proto__.removeItem = jest.fn();
+  });
+
+  describe('saveItem', () => {
+    it('calls localStorage setItem', () => {
+      saveItem('key', 'value');
+
+      expect(localStorage.setItem).toBeCalledWith('key', 'value');
+    });
+  });
+
+  describe('loadItem', () => {
+    it('calls localStorage getItem', () => {
+      loadItem('key');
+
+      expect(localStorage.getItem).toBeCalledWith('key');
+    });
+  });
+
+  describe('removeItem', () => {
+    it('calls localStorage removeItem', () => {
+      removeItem('key');
+
+      expect(localStorage.removeItem).toBeCalledWith('key');
+    });
+  });
+});
